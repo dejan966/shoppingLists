ea82f76f4b3aef288f11cdfe7a2f0c70da53dd27
dejan966 <ultimate24208@gmail.com>
2024-02-15 19:23:56 +0100

    Route for updating the list items

--- src/app/api/shoppingLists/route.ts
+++ src/app/api/shoppingLists/route.ts
@@ -10,6 +10,17 @@ export async function GET() {
   return Response.json(shoppingLists)
 }
 
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: number } },
+) {
+  const body = await request.json()
+  const { item } = body
+
+  shoppingLists[0].item[params.id] = item
+  return Response.json(shoppingLists[0])
+}
+
 export async function DELETE(request: Request) {
   const itemToDelete = request.body?.toString()
 
