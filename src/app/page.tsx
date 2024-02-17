00a1c96477078fa59076387bde90310c7bb55d78
dejan966 <ultimate24208@gmail.com>
2024-02-17 13:37:18 +0100

    Editing items in the list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { ShoppingListType } from '@/models/shoppingList'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
+  const addItemRef = useRef<HTMLInputElement>(null)
   const [checked, setChecked] = useState(true)
   const [shoppingLists, setShoppingLists] = useState<ShoppingListType[]>([])
 
@@ -15,6 +16,29 @@ export default function Home() {
   useEffect(() => {
     getShoppingLists()
   }, [])
+
+  const edit = async (event: any, id: number, i: number) => {
+    const timer = setTimeout(async () => {
+      editItem(event.target.value, id, i)
+    }, 2000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }
+
+  const editItem = async (item: string, id: number, index: number) => {
+    const res = await axios.patch(`api/shoppingLists/${id}`, {
+      newItem: item,
+      itemIndex: index,
+    })
+  }
+
+  const addItem = async () => {
+    const res = await axios.post('api/shoppingLists', {
+      newItem: addItemRef.current!.value,
+    })
+  }
 
   return (
     <div className="text-center w-full min-h-screen bg-white p-24">
@@ -40,6 +64,9 @@ export default function Home() {
                   <input
                     type="text"
                     name="item"
+                    onChange={(event) => {
+                      edit(event, shoppingList.id, i)
+                    }}
                     defaultValue={item}
                     className="border-t-0 border-l-0 border-r-0"
                   />
@@ -58,12 +85,14 @@ export default function Home() {
                   type="text"
                   name="newItem"
                   id="newItem"
+                  ref={addItemRef}
                   placeholder="Add item"
                   className="w-[17.5rem] border-t-0 border-l-0 border-r-0 hover:border-y-black col-span-2"
                 />
                 <button
                   type="button"
                   className="w-20 h-12 text-sm text-white bg-blue-800 hover:bg-blue-500 rounded-lg"
+                  onClick={addItem}
                 >
                   Add
                 </button>
